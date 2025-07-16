dc7a4ec7b7a2c482a80cabf24ecfb3674b645522
minu <min9288@gmail.com>
2025-07-17 00:24:14 +0900

    chore: rest-docs & openApi gradle 셋팅

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,12 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.4.1"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.asciidoctor.jvm.convert") version "3.3.2"
+	id("com.epages.restdocs-api-spec") version "0.19.4"
 }
+
+val snippetsDir by extra { file("build/generated-snippets") }
+val asciidoctorExt: Configuration by configurations.creating
 
 fun getGitHash(): String {
 	return providers.exec {
@@ -38,6 +43,14 @@ dependencies {
     // DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
+	// Docs
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
+	asciidoctorExt("org.springframework.restdocs:spring-restdocs-asciidoctor:3.0.0")
+	
+	// RestDocs API Spec
+	testImplementation("com.epages:restdocs-api-spec:0.19.4")
+	testImplementation("com.epages:restdocs-api-spec-mockmvc:0.19.4")
+
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.boot:spring-boot-testcontainers")
@@ -47,6 +60,39 @@ dependencies {
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
-	systemProperty("user.timezone", "UTC")
+	outputs.dir(snippetsDir)
+}
+
+tasks.named("asciidoctor") {
+	dependsOn(tasks.test)
+	
+	doFirst {
+		delete(file("src/main/resources/static/docs"))
+	}
+}
+
+tasks.getByName<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
+	dependsOn("asciidoctor")
+	from("${layout.buildDirectory.dir("docs/asciidoc")}") {
+		into("static/docs")
+	}
+}
+
+tasks.register<Copy>("copyDocument") {
+	dependsOn("asciidoctor")
+	from(layout.buildDirectory.dir("docs/asciidoc"))
+	into("src/main/resources/static/docs")
+}
+
+tasks.getByName("build") {
+	dependsOn("copyDocument")
+}
+
+extensions.configure<com.epages.restdocs.apispec.gradle.OpenApi3Extension> {
+	this.setServer("http://localhost:8080")
+	title = "Spring RestDocs to Redoc"
+	description = "Spring RestDocs를 Redoc문서로 변환한다."
+	version = "0.0.1"
+	format = "yaml"
+	outputDirectory = "src/main/resources/static/docs"
 }
