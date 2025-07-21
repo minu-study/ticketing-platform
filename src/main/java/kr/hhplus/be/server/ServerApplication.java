190d603f7912293e0d6855b5f27db23abaf221c7
minu <min9288@gmail.com>
2025-07-22 00:49:36 +0900

    feat: JPA Audition 및 비동기 활성화

--- src/main/java/kr/hhplus/be/server/ServerApplication.java
+++ src/main/java/kr/hhplus/be/server/ServerApplication.java
@@ -2,8 +2,12 @@ package kr.hhplus.be.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
 public class ServerApplication {
 
 	public static void main(String[] args) {
