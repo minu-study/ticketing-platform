2d6851e5b247bf466e289754e6d9547ec9502b59
minu <min9288@gmail.com>
2025-07-22 00:21:31 +0900

    feat: 계정 및 결제관련 에러코드 추가

--- src/main/java/kr/hhplus/be/server/common/exception/ErrorCode.java
+++ src/main/java/kr/hhplus/be/server/common/exception/ErrorCode.java
@@ -7,15 +7,16 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-    AUTH001("AUTH001", "인증에 실패했습니다."),
+    AUTH001("AUTH001", "존재하지 않는 계정입니다."),
     AUTH002("AUTH002", "이미 존재하는 계정 정보 입니다."),
+    AUTH003("AUTH003", "인증 헤더가 비어있습니다."),
 
     DB001("DB001", "존재하지 않는 데이터입니다."),
 
-    MEMBER001("MEMBER001", "포인트 적립에 실패했습니다. 포인트는 양수만 가능합니다."),
-
     PAYMENT001("PAYMENT001", "결제에 실패했습니다. 결제금이 상품 가격보다 적습니다."),
     PAYMENT002("PAYMENT002", "지원하지 않는 결제 수단입니다."),
+    PAYMENT003("PAYMENT003", "결제금 충전에 실패했습니다. 결제금액은 양수만 가능합니다."),
+    PAYMENT004("PAYMENT004", "결제에 실패했습니다.결제금은 양수만 가능합니다."),
     ;
 
     private final String code;
