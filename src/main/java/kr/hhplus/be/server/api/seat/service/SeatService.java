cbc53c7ca31df2e4470d4ea089ac3abe073e30a3
minu <min9288@gmail.com>
2025-07-24 00:03:09 +0900

    feat: 좌석 동기화 메서드 추가

--- src/main/java/kr/hhplus/be/server/api/seat/service/SeatService.java
+++ src/main/java/kr/hhplus/be/server/api/seat/service/SeatService.java
@@ -3,7 +3,9 @@ package kr.hhplus.be.server.api.seat.service;
 import kr.hhplus.be.server.api.queue.service.QueueService;
 import kr.hhplus.be.server.common.util.CommonUtil;
 import kr.hhplus.be.server.domain.seat.dto.SeatDto;
+import kr.hhplus.be.server.domain.seat.entity.Seat;
 import kr.hhplus.be.server.domain.seat.repository.SeatRepository;
+import kr.hhplus.be.server.domain.seat.vo.SeatStatusEnums;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -36,5 +38,25 @@ public class SeatService {
                 .build();
     }
 
+
+    @Transactional
+    public void syncSeats(LocalDateTime now) {
+
+        List<Seat> expiredHoldSeats = seatRepository
+                .findByStatusAndHoldExpiresAtBefore(SeatStatusEnums.HOLD.getStatus(), now);
+
+        if (!expiredHoldSeats.isEmpty()) {
+
+            for (Seat seat : expiredHoldSeats) {
+                try {
+                        seat.setStatus(SeatStatusEnums.AVAILABLE.getStatus());
+                        seat.setHoldExpiresAt(null);
+                        seatRepository.save(seat);
+                    } catch(Exception e) {
+                        log.error("syncSeats, update error, seat id : {}", seat.getId());
+                    }
+            }
+        }
+    }
 
 }
