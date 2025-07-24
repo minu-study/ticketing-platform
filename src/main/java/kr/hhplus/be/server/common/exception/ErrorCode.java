020d553eb195d0a8097fb85478d9db7b7f559dbe
minu <min9288@gmail.com>
2025-07-24 22:25:54 +0900

    feat: 예약 및 결제 에러코드 추가

--- src/main/java/kr/hhplus/be/server/common/exception/ErrorCode.java
+++ src/main/java/kr/hhplus/be/server/common/exception/ErrorCode.java
@@ -17,10 +17,15 @@ public enum ErrorCode {
 
     DB001("DB001", "존재하지 않는 데이터입니다."),
 
+    RESERVATION001("RESERVATION001", "예약가능한 좌석이 아닙니다."),
+
     PAYMENT001("PAYMENT001", "결제에 실패했습니다. 결제금이 상품 가격보다 적습니다."),
     PAYMENT002("PAYMENT002", "지원하지 않는 결제 수단입니다."),
     PAYMENT003("PAYMENT003", "결제금 충전에 실패했습니다. 결제금액은 양수만 가능합니다."),
     PAYMENT004("PAYMENT004", "결제에 실패했습니다.결제금은 양수만 가능합니다."),
+    PAYMENT005("PAYMENT005", "대기 예약만 결제가 가능합니다,  예약 상태를 확인해주세요."),
+    PAYMENT007("PAYMENT007", "결제 처리 중 오류가 발생했습니다."),
+    PAYMENT008("PAYMENT008", "취소가 불가한 예약입니다."),
     ;
 
     private final String code;
